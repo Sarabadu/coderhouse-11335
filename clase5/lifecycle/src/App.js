300304fa75a1ff9d34fa8a945c0fea053f4b89a4
Juan Pablo Garcia Ripa <j.garcia.ripa@altoros.com>
2021-03-12 09:42:11 -0300

    clase 5 add old cases

--- clase5/lifecycle/src/App.js
+++ clase5/lifecycle/src/App.js
@@ -4,9 +4,10 @@ import { MyComponent } from "./components/MyComponent";
 
 function App() {
   const [montado, setMontado] = useState(true);
-
+  const juan = { name: "juan" };
   return (
     <div className="App">
+      <h3>Este se desmonta</h3>
       {montado ? (
         <MyComponent
           onClick1={(num) => console.log(`my componente me dio un: ${num}`)}
@@ -15,6 +16,17 @@ function App() {
       ) : null}
 
       <button onClick={() => setMontado(!montado)}>Desmontar</button>
+
+      <div style={{ marginTop: 20 }}>
+        <h3>Este no se desmonta</h3>
+        <MyComponent
+          texto="text"
+          numero={4}
+          bool={false}
+          obj={juan}
+          fun={() => console.log("yo soy fun")}
+        ></MyComponent>
+      </div>
     </div>
   );
 }
